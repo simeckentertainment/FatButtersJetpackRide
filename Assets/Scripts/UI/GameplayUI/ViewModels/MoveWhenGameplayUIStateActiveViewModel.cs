d2dc085078cd858ecc3ec9f2205da22b5b86de29
GastricBypass <dsp45@case.edu>
2026-01-30 16:38:26 -0800

    Made the gameplay UI menus slide up and down like the level select menu does. This required changing the SlideableViewModel to work during Update instead of FixedUpdate because FixedUpdate doesn't run at all when Time.timeScale = 0. Also I'm using unscaledDeltaTime instead of regular deltaTime for the same reason.

---
+++ Assets/Scripts/UI/GameplayUI/ViewModels/MoveWhenGameplayUIStateActiveViewModel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MoveWhenGameplayUIStateActiveViewModel : SinusoidalSlideableViewModel<GameplayUIModel>
+{
+    [SerializeField] private GameplayUIState uiState;
+
+    protected override bool IsActive()
+    {
+        return uiState == Model.UIState;
+    }
+}
