8e7154c56954260c65708cfc641aea0d20be8d92
sedex0203 <wink.dev114@gmail.com>
2026-02-06 14:56:37 +0545

    Added visual indicator on fuel gauge
    FuelGaugeViewModel-> Update()

--- Assets/Scripts/UI/GameplayUI/ViewModels/FuelGaugeViewModel.cs
+++ Assets/Scripts/UI/GameplayUI/ViewModels/FuelGaugeViewModel.cs
@@ -1,25 +1,18 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class FuelGaugeViewModel : ImageViewModel<GameplayUIModel>
 {
     [SerializeField] private Sprite[] FuelGaugeColors;
-    [SerializeField] private Image barImage;
     [SerializeField] private float colorPingPongLength = 0.3f;
-    [SerializeField] Player player;
 
     protected override void OnModelChanged()
     {
-
-        Image.fillAmount = Model.FuelPercent;
-
-
+        Image.fillAmount =  Model.FuelPercent;
         base.OnModelChanged();
     }
 
     protected override Sprite GetSprite()
     {
-
         if (Model.FuelPercent > 0.5f)
         {
             Image.color = Color.white;
@@ -34,31 +27,13 @@ public class FuelGaugeViewModel : ImageViewModel<GameplayUIModel>
         {
             return FuelGaugeColors[2];
         }
-
     }
 
     private void Update()
     {
-
         if (Model.FuelPercent < 0.25f)
         {
             Image.color = Color.Lerp(Color.white, Color.black, Mathf.PingPong(Time.time, colorPingPongLength)); // color PingPong
         }
-       
-
-        // visual indicator on the fuel gauge when player trigger ball
-        // just comment out the below code if you want to see the magic.
-        
-        
-        // if (player.hasTemporaryBall || player.hasPermaBall)
-        // {
-        //     Image.color = Color.Lerp(Color.white, Color.black, Mathf.PingPong(Time.time, colorPingPongLength)); // color PingPong the inner bar
-        //     barImage.color = Color.Lerp(Color.white, Color.black, Mathf.PingPong(Time.time, colorPingPongLength)); // color PingPong the outer bar
-             /* Comment out both line of code to ping pong both inner and outer bar. */
-        // }
-      
-
-        
-
     }
 }
