58a9555f6e82a7ac605d602e738967e04f60d1bd
GastricBypass <dsp45@case.edu>
2026-02-06 14:09:09 -0800

    Made the Age Gate screen in MVVM. The only behavior that's a little different is the fade out/in thing, but that never worked well anyways. Added a prefab for a dropdown without a label which I make use of here.

---
+++ Assets/Scripts/UI/AgeGateMenu/ViewModels/YearDropdownViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YearDropdownViewModel : DropdownViewModel<AgeGateMenuModel>
+{
+    [SerializeField] private int yearsToGoBack = 100;
+
+    private void Start()
+    {
+        Dropdown.ClearOptions();
+        var yearOptions = new List<string>();
+        yearOptions.Add("Year");
+        for (int i = 0; i < yearsToGoBack; i++)
+        {
+            yearOptions.Add((DateTime.Now.Year - i).ToString());
+        }
+
+        Dropdown.AddOptions(yearOptions);
+    }
+
+    protected override void OnDropdownChanged(int index)
+    {
+        Model.Year = int.Parse(Dropdown.options[index].text);
+    }
+}
