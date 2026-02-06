17662de794a79ccf12bcc1e02adb4cf60db9d286
sedex0203 <wink.dev114@gmail.com>
2026-02-06 16:13:19 +0545

    fuel gauge guard

--- Assets/Scripts/UI/GameplayUI/ViewModels/FuelGaugeViewModel.cs
+++ Assets/Scripts/UI/GameplayUI/ViewModels/FuelGaugeViewModel.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FuelGaugeViewModel : ImageViewModel<GameplayUIModel>
 {
     [SerializeField] private Sprite[] FuelGaugeColors;
     [SerializeField] private float colorPingPongLength = 0.3f;
+   //[SerializeField] Image barImage;
 
     protected override void OnModelChanged()
     {
@@ -34,6 +36,17 @@ public class FuelGaugeViewModel : ImageViewModel<GameplayUIModel>
         if (Model.FuelPercent < 0.25f)
         {
             Image.color = Color.Lerp(Color.white, Color.black, Mathf.PingPong(Time.time, colorPingPongLength)); // color PingPong
+        // visual indicator on the fuel gauge when player trigger ball
+        // just comment out the below code if you want to see the magic.
+        
+        
+        // if (player.hasTemporaryBall || player.hasPermaBall)
+        // {
+        //     Image.color = Color.Lerp(Color.white, Color.black, Mathf.PingPong(Time.time, colorPingPongLength)); // color PingPong the inner bar
+        //     barImage.color = Color.Lerp(Color.white, Color.black, Mathf.PingPong(Time.time, colorPingPongLength)); // color PingPong the outer bar 
+             /* Comment out both line of code to ping pong both inner and outer bar. */
+        // }
+       
         }
     }
 }
