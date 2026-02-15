80c932ccb49c01fdb48fb90e82732f4445f738ed
Weeble Wobble <seansimonanimation@gmail.com>
2026-02-14 17:42:54 -0700

    Fixed ball tagging issue
    Also removed some commented legacy code.

--- Assets/Scripts/Gameplay/effects/BallRandomizer.cs
+++ Assets/Scripts/Gameplay/effects/BallRandomizer.cs
@@ -17,12 +17,6 @@ public class BallRandomizer : MonoBehaviour
         BallData = Instantiate(objectList[rng],transform.position, Quaternion.identity);
         BallData.transform.parent = gameObject.transform;
         BallData.tag = "Ball";
-
-        //meshFilter = GetComponent<MeshFilter>();
-        //meshRenderer = GetComponent<MeshRenderer>();
-
-        //meshFilter.mesh = objectList[rng].gameObject.GetComponent<MeshFilter>().mesh;
-        //meshRenderer.material = objectList[rng].gameObject.GetComponent<MeshRenderer>().material;
     }
 
     // Update is called once per frame
