41d7ea7a3590968da9e235078bcb93c6a2af7d7d
GastricBypass <dsp45@case.edu>
2026-03-02 10:48:54 -0800

    Added a flashing jetpack warning whenever you try to use the jetpack in a no-fly zone.
    There's a bit of an anti-pattern here in that the JetpackWarningFlashingViewModel checks things on update rather than with an event. I think this is acceptable because of the other patterns in player.
    
    # Conflicts:
    #	Assets/Objects/UI/Prefabs/GameplayUI.prefab

---
+++ Assets/Scripts/UI/GameplayUI/ViewModels/JetpackWarningFlashingViewModel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class JetpackWarningFlashingViewModel : ImageViewModel<GameplayUIModel>
+{
+    [SerializeField] private float colorPingPongLength = 0.3f;
+
+    private void FixedUpdate()
+    {
+        var clearWhite = new Color(1, 1, 1, 0);
+        if (Model.PlayerIsUsingJetpack)
+        {
+            Image.color = Color.Lerp(Color.white, clearWhite, Mathf.PingPong(Time.time, colorPingPongLength)); // color PingPong
+        }
+        else
+        {
+            Image.color = clearWhite;
+        }
+    }
+}
