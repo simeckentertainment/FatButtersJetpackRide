e007aff3b335208fb33fb97c7067e3b2792766cc
GastricBypass <dsp45@case.edu>
2026-03-03 10:52:18 -0800

    Fixed a new bug where pickups could be double counted when multiple collision reporters (body parts) collide with the pickup simultaneously

--- Assets/Scripts/Gameplay/PuzzleObjectScripts/Powerup/Pickup.cs
+++ Assets/Scripts/Gameplay/PuzzleObjectScripts/Powerup/Pickup.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public abstract class Pickup : MonoBehaviour
 {
+    private bool isPickedUp = false; // fixes a case where collision reporters collide with the pickup simultaneously
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent<PlayerCollisionReporter>(out var playerCollision))
+        if (!isPickedUp && other.TryGetComponent<PlayerCollisionReporter>(out var playerCollision))
         {
+            isPickedUp = true;
             var player = playerCollision.player;
             if (player.IsAlive)
             {
