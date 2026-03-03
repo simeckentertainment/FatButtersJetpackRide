cd29106aef45c3af2a949023c3dad27fa646627b
GastricBypass <dsp45@case.edu>
2026-03-03 10:21:30 -0800

    Fixed bug where butters' head wouldn't trigger pickups.
    This is because the Player object's collider is smaller than all of the PlayerCollisionReporters' colliders. Pickup is now looking for PlayerCollisionReporters.

--- Assets/Scripts/Gameplay/PuzzleObjectScripts/Powerup/Pickup.cs
+++ Assets/Scripts/Gameplay/PuzzleObjectScripts/Powerup/Pickup.cs
@@ -4,8 +4,9 @@ public abstract class Pickup : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent<Player>(out var player))
+        if (other.TryGetComponent<PlayerCollisionReporter>(out var playerCollision))
         {
+            var player = playerCollision.player;
             if (player.IsAlive)
             {
                 OnPlayerTriggerEnter(player);
