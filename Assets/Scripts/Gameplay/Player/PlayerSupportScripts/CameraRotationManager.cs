869ee07546cdae8574e4bb5b4236547423d94fa7
Weeble Wobble <seansimonanimation@gmail.com>
2026-03-05 18:51:31 -0700

    Wrote wobble algorithm

--- Assets/Scripts/Gameplay/Player/PlayerSupportScripts/CameraRotationManager.cs
+++ Assets/Scripts/Gameplay/Player/PlayerSupportScripts/CameraRotationManager.cs
@@ -27,12 +27,14 @@ public class CameraRotationManager : MonoBehaviour
     {
         if(deviceType == DeviceType.Stationary){ return;} //Don't need to handle rotation for stationary devices!
         //We're manually taking over camera rotations because letting Cinemachine do it was leading to unpredictable results.
+        if(!runningWobble){
         virtualCam.localRotation = Quaternion.Euler(0.0f,0.0f,player.input.aimAngle*2); 
+        }
 
         //always be watching for the max speed a player can achieve. //TODO: Refactor this based on physics calculations times thrust stat.
         maxSpeed = WatchForMaxSpeed();
 
-        if(!runningWobble && (player.OtherObjectTouch || player.GroundTouch) && player.rb.linearVelocity.magnitude > maxSpeed * 0.6f)
+        if(!runningWobble && (player.OtherObjectTouch || player.GroundTouch) && player.rb.linearVelocity.magnitude > maxSpeed * 0.35f)
         {
             InitiateWobble();
         }
@@ -89,9 +91,106 @@ public class CameraRotationManager : MonoBehaviour
         if(wobbleCounter > wobbleMaxCount)
         {
             runningWobble = false;
-            return;
+            virtualCam.localRotation = Quaternion.Euler(0.0f,0.0f,player.input.aimAngle*2); //reset to active angle tracking.
+            wobblePoints = new float[1]; //clear that puppy out.
+            return; //kicks us out of wobble madness if we're done.
         }
-        //TODO: Finish this. Use the ball arc from Zoodia's boss as a template.
+
+        if(wobblePoints.Length == 5){virtualCam.localRotation = Quaternion.Euler(run5Wobble());}
+        if(wobblePoints.Length == 7){virtualCam.localRotation = Quaternion.Euler(run7Wobble());}
+        //I'm sure there's a better way to do this than writing 2 methods that do the same thing slightly differently, but I just wanna get it done. Maybe I can come back to this and rethink it later.
+    }
+
+    Vector3 run5Wobble()
+    { // threshold points are .25, .5, .75, and 1
+        Vector3 outRot;
+        float percentComplete = wobbleCounter/wobbleMaxCount;
+        if(percentComplete < 0.25f) //This code is ugly as sin but it works a treat.
+        {
+            outRot = Vector3.Lerp(
+                    new Vector3(0.0f,0.0f,wobblePoints[0]),
+                    new Vector3(0.0f,0.0f,wobblePoints[1]),
+                    Helper.RemapToBetweenZeroAndOne(0.0f,0.25f,percentComplete
+                    )
+                );
+        }else if (percentComplete > 0.25f && percentComplete < 0.5f)
+        {
+            outRot = Vector3.Lerp(
+                new Vector3(0.0f,0.0f,wobblePoints[1]),
+                new Vector3(0.0f,0.0f,wobblePoints[2]),
+                Helper.RemapToBetweenZeroAndOne(0.25f,0.5f,percentComplete)
+            );
+
+
+        }else if (percentComplete > 0.5f && percentComplete < 0.75f)
+        {
+            outRot = Vector3.Lerp(
+                new Vector3(0.0f,0.0f,wobblePoints[2]),
+                new Vector3(0.0f,0.0f,wobblePoints[3]),
+                Helper.RemapToBetweenZeroAndOne(0.5f,0.75f,percentComplete)
+            );            
+        } else
+        {
+            outRot = Vector3.Lerp(
+                new Vector3(0.0f,0.0f,wobblePoints[3]),
+                new Vector3(0.0f,0.0f,wobblePoints[4]),
+                Helper.RemapToBetweenZeroAndOne(0.75f,1.0f,percentComplete)
+            ); //this line's ugly as sin.        
+        }
+        return outRot;
+    } 
+
+    Vector3 run7Wobble()
+    { //threshold points are .16, .33, .45, .65, .82, and 1
+        Vector3 outRot;
+        float percentComplete = wobbleCounter/wobbleMaxCount;
+        if(percentComplete < 0.16f) //This code is ugly as sin but it works a treat.
+        {
+            outRot = Vector3.Lerp(
+                    new Vector3(0.0f,0.0f,wobblePoints[0]),
+                    new Vector3(0.0f,0.0f,wobblePoints[1]),
+                    Helper.RemapToBetweenZeroAndOne(0.0f,0.16f,percentComplete
+                    )
+                );
+        }else if (percentComplete > 0.16f && percentComplete < 0.33f)
+        {
+            outRot = Vector3.Lerp(
+                new Vector3(0.0f,0.0f,wobblePoints[1]),
+                new Vector3(0.0f,0.0f,wobblePoints[2]),
+                Helper.RemapToBetweenZeroAndOne(0.33f,0.45f,percentComplete)
+            );
+
+
+        }else if (percentComplete > 0.33f && percentComplete < 0.45f)
+        {
+            outRot = Vector3.Lerp(
+                new Vector3(0.0f,0.0f,wobblePoints[2]),
+                new Vector3(0.0f,0.0f,wobblePoints[3]),
+                Helper.RemapToBetweenZeroAndOne(0.5f,0.75f,percentComplete)
+            );            
+        }else if (percentComplete > 0.45f && percentComplete < 0.65f) 
+        {
+            outRot = Vector3.Lerp(
+                new Vector3(0.0f,0.0f,wobblePoints[3]),
+                new Vector3(0.0f,0.0f,wobblePoints[4]),
+                Helper.RemapToBetweenZeroAndOne(0.75f,1.0f,percentComplete)
+            ); //this line's ugly as sin.        
+        } else if (percentComplete > 0.65f && percentComplete < 0.82f) 
+        {
+            outRot = Vector3.Lerp(
+                new Vector3(0.0f,0.0f,wobblePoints[4]),
+                new Vector3(0.0f,0.0f,wobblePoints[5]),
+                Helper.RemapToBetweenZeroAndOne(0.65f,0.82f,percentComplete)
+            ); //this line's ugly as sin.        
+        } else
+        {
+            outRot = Vector3.Lerp(
+                new Vector3(0.0f,0.0f,wobblePoints[5]),
+                new Vector3(0.0f,0.0f,wobblePoints[6]),
+                Helper.RemapToBetweenZeroAndOne(0.82f,1.0f,percentComplete)
+            ); //this line's ugly as sin.        
+        }  
+        return outRot;         
     }
 
 }
