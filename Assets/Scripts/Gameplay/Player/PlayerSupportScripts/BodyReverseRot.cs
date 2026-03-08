fdf63c398106900398c22609c50607ec1da7db41
Weeble Wobble <seansimonanimation@gmail.com>
2026-03-08 15:13:53 -0700

    Fixed a bug where the holster would rotate along with the jetpack.
    Also made some adjustments to make the fuel guage a little more forgiving.

--- Assets/Scripts/Gameplay/Player/PlayerSupportScripts/BodyReverseRot.cs
+++ Assets/Scripts/Gameplay/Player/PlayerSupportScripts/BodyReverseRot.cs
@@ -14,14 +14,20 @@ public class BodyReverseRot : MonoBehaviour
         // float zAngle = OGButtersTrans.localEulerAngles.z;
         // float invertedAngle = (-zAngle + offset);
 
-        if (affectAxis == AffectAxis.Z){
+        if (affectAxis == AffectAxis.Z)
+        {
             transform.rotation = Quaternion.Euler(0, 0, (OGButtersTrans.rotation.z * -1) + offset);
-        } else if (affectAxis == AffectAxis.Y){
-            transform.rotation = Quaternion.Euler(0f, (OGButtersTrans.rotation.z * -1) + offset, 0f);
-        }else{
+        }
+        else if (affectAxis == AffectAxis.Y)
+        {
             transform.rotation = Quaternion.Euler(0f, (OGButtersTrans.rotation.z * -1) + offset, 0f);
         }
-
+        else
+        {
+            transform.rotation = Quaternion.Euler(0f, (OGButtersTrans.rotation.z * -1) + offset, 0f);
+        }
+            //This shouldn't work. It works but it shouldn't. The connections I thought made it work
+            //are broken now, much like my spirit. ~Randy
     }
 
         public enum AffectAxis {X,Y,Z};
