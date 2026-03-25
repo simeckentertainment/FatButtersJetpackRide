b0f7bf070bb27acdd825c4340dd07e9f5203eed9
GastricBypass <dsp45@case.edu>
2026-03-25 10:31:41 -0700

    Fixed the volume levels not updating while moving the sliders. MasterVolumeLevel is now correctly acting as a limiter for the other volume sliders.

--- Assets/Scripts/archive/MusicManager.cs
+++ Assets/Scripts/archive/MusicManager.cs
@@ -16,7 +16,7 @@ public class MusicManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         audioSource.volume = collectibleData.MusicVolumeLevel;
     }
