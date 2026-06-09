1895fb5f7afa04d7823d0b50d136d8df01f90e95
GastricBypass <dsp45@case.edu>
2026-06-09 10:45:27 -0700

    Level select button size now scales with distance to camera. It's not perfect but it's a good start.

--- Assets/Scripts/UI/LevelSelectUI/ViewModels/LevelSelectButtonViewModel.cs
+++ Assets/Scripts/UI/LevelSelectUI/ViewModels/LevelSelectButtonViewModel.cs
@@ -5,12 +5,19 @@ public class LevelSelectButtonViewModel : ButtonViewModel<LevelSelectUIModel>
     [SerializeField] public LevelButtonIDHolder levelId;
     [SerializeField] public Camera cam;
     [SerializeField] public Vector3 positionOffset;
+    [SerializeField] public float scaleMultiplier = 1;
+    [SerializeField] public float scaleDistanceMultiplier = 1;
+    [SerializeField] public float scaleDistanceOffset;
 
     private CollectibleData collectibleData => SaveManager.Instance.collectibleData;
 
     private void Update()
     {
         transform.position = cam.WorldToScreenPoint(levelId.transform.position) + positionOffset;
+        var distanceToCamera = Vector3.Distance(cam.transform.position, levelId.transform.position);
+        var inverseDistance = (scaleDistanceMultiplier / distanceToCamera) + scaleDistanceOffset;
+        transform.localScale = new Vector3(inverseDistance, inverseDistance, inverseDistance) * scaleMultiplier;
+        // small ones need to be bigger, big ones need to be smaller
     }
 
     protected override bool IsEnabled()
