6a726402d4a8efc8c4e8867a798d8e05e9a094f2
GastricBypass <dsp45@case.edu>
2026-06-11 09:10:24 -0700

    The buttons now scale based on the distance to the camera plane instead of to the camera itself.

--- Assets/Scripts/UI/LevelSelectUI/ViewModels/LevelSelectButtonViewModel.cs
+++ Assets/Scripts/UI/LevelSelectUI/ViewModels/LevelSelectButtonViewModel.cs
@@ -14,10 +14,13 @@ public class LevelSelectButtonViewModel : ButtonViewModel<LevelSelectUIModel>
     private void Update()
     {
         transform.position = cam.WorldToScreenPoint(levelId.transform.position) + positionOffset;
-        var distanceToCamera = Vector3.Distance(cam.transform.position, levelId.transform.position);
+
+        Plane plane = new Plane();
+        plane.SetNormalAndPosition(cam.transform.forward, cam.transform.position);
+
+        var distanceToCamera = plane.GetDistanceToPoint(levelId.transform.position);
         var inverseDistance = (scaleDistanceMultiplier / distanceToCamera) + scaleDistanceOffset;
         transform.localScale = new Vector3(inverseDistance, inverseDistance, inverseDistance) * scaleMultiplier;
-        // small ones need to be bigger, big ones need to be smaller
     }
 
     protected override bool IsEnabled()
